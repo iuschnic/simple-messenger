f2e0d866efe9d9333caed82ea3890cdfc7b01483
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-03-30 17:56:00 +0300

    #21 модели хранилища

---
+++ src/backend/Main.DB/Models/MessageDb.cs
@@ -0,0 +1,55 @@
+﻿using Main.DB.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Main.DB.Models;
+
+public class MessageDb
+{
+    internal MessageDb() { }
+    public MessageDb(
+        ulong messageNumber,
+        Guid chatId,
+        Guid senderId,
+        string text,
+        MessageTypeDb type,
+        ulong? replyToMessageNumber = null,
+        Guid? forwardedFromUserId = null)
+    {
+        MessageNumber = messageNumber;
+        ChatId = chatId;
+        SenderId = senderId;
+        Text = text;
+        Type = type;
+        ReplyToMessageNumber = replyToMessageNumber;
+        ForwardedFromUserId = forwardedFromUserId;
+        CreatedAt = DateTime.UtcNow;
+        EditedAt = DateTime.UtcNow;
+        Deleted = false;
+        Version = 1;
+    }
+    [Required]
+    public ulong MessageNumber { get; set; }
+    [Required]
+    public Guid ChatId { get; set; }
+    [Required]
+    public Guid SenderId { get; set; }
+    [Required]
+    public string Text { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    public DateTime EditedAt { get; set; }
+    [Required]
+    public bool Deleted { get; set; }
+    [Required]
+    public ulong Version { get; set; }
+    [Required]
+    public MessageTypeDb Type { get; set; }
+
+    public ulong? ReplyToMessageNumber { get; set; }
+    public Guid? ForwardedFromUserId { get; set; }
+    public ChatDb? Chat { get; set; }
+    public UserDb? Sender { get; set; }
+    public MessageDb? ReplyToMessage { get; set; }
+    public UserDb? ForwardedFromUser { get; set; }
+}
---
+++ src/backend/Main.DB/Models/UserDb.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Main.DB.Models;
+
+public class UserDb
+{
+    internal UserDb() { }
+    public UserDb(Guid id, string uniqueName, string displayedName)
+    {
+        Id = id;
+        UniqueName = uniqueName;
+        DisplayedName = displayedName;
+    }
+    [Required]
+    public Guid Id { get; set; }
+    [Required]
+    public string UniqueName { get; set; }
+    [Required]
+    public string DisplayedName { get; set; }
+
+    public ICollection<ContactDb> Contacts { get; set; } = [];
+    public ICollection<ChatUserDb> UserChats { get; set; } = [];
+    public ICollection<MessageDb> SentMessages { get; set; } = [];
+}
