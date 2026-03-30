3a6cf63ee9b46d7341108a80c523f314357807ae
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-03-30 14:23:08 +0300

    #21 доменные модели сервиса Main

---
+++ src/backend/Main.BL/Models/Message.cs
@@ -0,0 +1,29 @@
+﻿using Main.BL.Enums;
+namespace Main.BL.Models;
+
+public class Message(
+    ulong messageNumber,
+    Guid chatId,
+    Guid senderId,
+    string text,
+    DateTime createdAt,
+    DateTime editedAt,
+    bool deleted,
+    ulong version,
+    MessageType type,
+    ulong? replyToMessageNumber,
+    Guid? forwardedFromUserId
+)
+{
+    public ulong MessageNumber { get; } = messageNumber;
+    public Guid ChatId { get; } = chatId;
+    public Guid SenderId { get; } = senderId;
+    public string Text { get; } = text;
+    public DateTime CreatedAt { get; } = createdAt;
+    public DateTime EditedAt { get; } = editedAt;
+    public bool Deleted { get; } = deleted;
+    public ulong Version { get; } = version;
+    public MessageType Type { get; } = type;
+    public ulong? ReplyToMessageNumber { get; } = replyToMessageNumber;
+    public Guid? ForwardedFromUserId { get; } = forwardedFromUserId;
+}
---
+++ src/backend/Main.BL/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Main.BL.Models;
+
+public class User(Guid id, 
+    string uniqueName, 
+    string displayedName)
+{
+    public Guid Id { get; } = id;
+    public string UniqueName { get; } = uniqueName;
+    public string DisplayedName { get; } = displayedName;
+}
