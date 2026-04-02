e772718b562f2775f335b1d30124ffb00a847ece
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-02 17:58:48 +0300

    #21 первая версия репозитория чатов

---
+++ src/backend/Main.BL/OutPorts/IChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using Main.BL.Models;
+
+namespace Main.BL.OutPorts;
+
+public interface IChatRepository
+{
+    Task<Chat?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Chat>> GetUserChatsAsync(Guid userId);
+    Task<Chat> CreateAsync(Chat chat);
+    Task UpdateNameAsync(Guid chatId, string newName);
+    Task DeleteAsync(Guid id);
+    Task<bool> ExistsAsync(Guid id);
+}
