a084f7903402226d941eb1dee275a0b98619d9f6
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-02 16:03:22 +0300

    #21 конвертеры сущностей между БД и БЛ

---
+++ src/backend/Main.DB/Converters/ChatUserConverter.cs
@@ -0,0 +1,22 @@
+﻿using Main.BL.Models;
+using Main.DB.Models;
+
+namespace Main.DB.Converters;
+
+public static class ChatUserConverter
+{
+    public static ChatUserDb ToDb(this ChatUser domain, Guid chatId)
+    {
+        return new ChatUserDb(
+            chatId,
+            domain.UserId,
+            domain.LastMessageRead);
+    }
+
+    public static ChatUser ToDomain(this ChatUserDb db)
+    {
+        return new ChatUser(
+            db.UserId,
+            db.LastMessageRead);
+    }
+}
