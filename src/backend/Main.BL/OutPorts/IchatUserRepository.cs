3e8fe1e2b44c7e4aad34b61a0bcaf57d0ebb8da9
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-04 14:16:03 +0300

    #21 репозиторий связей пользователей и чатов

---
+++ src/backend/Main.BL/OutPorts/IchatUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Main.BL.Models;
+
+namespace Main.BL.OutPorts;
+
+public interface IChatUserRepository
+{
+    Task<ChatUser?> GetAsync(Guid chatId, Guid userId);
+    Task<IEnumerable<ChatUser>> GetChatParticipantsAsync(Guid chatId);
+    Task<ulong> GetLastMessageReadAsync(Guid chatId, Guid userId);
+    Task<bool> TryAddAsync(Guid chatId, ChatUser chatUser);
+    Task<int> TryAddManyAsync(Guid chatId, IEnumerable<ChatUser> chatUsers);
+    Task<bool> TryUpdateLastMessageReadAsync(Guid chatId, Guid userId, ulong lastMessageRead);
+    Task<bool> TryRemoveAsync(Guid chatId, Guid userId);
+    Task<bool> ExistsAsync(Guid chatId, Guid userId);
+}
