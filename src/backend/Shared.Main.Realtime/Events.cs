4bc75b049e0b2581e6e4be8bb3005ba7c1fc2af3
Ktot17 <chikinpavel4@gmail.com>
2026-04-08 21:00:55 +0300

    #25 Добавил класс с событиями

---
+++ src/backend/Shared.Main.Realtime/Events.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Main.Realtime;
+
+public class Events
+{
+    public const string MessageReceived = "MessageReceived";
+    public const string MessageUpdated = "MessageUpdated";
+    public const string MessageRead = "MessageRead";
+    public const string UserChanged = "UserChanged";
+    public const string ChatCreated = "ChatCreated";
+    public const string ChatUpdated = "ChatUpdated";
+    public const string ChatUserJoined = "ChatUserJoined";
+    public const string ChatUserLeft = "ChatUserLeft";
+}
