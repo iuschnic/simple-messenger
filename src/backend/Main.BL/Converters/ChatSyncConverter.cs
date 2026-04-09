7b9d288cf29b3f6347400616226fd2f81c73f1be
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-09 15:01:12 +0300

    #21 интерфейсы сервисов бизнес логики

---
+++ src/backend/Main.BL/Converters/ChatSyncConverter.cs
@@ -0,0 +1,36 @@
+﻿using Main.BL.Models;
+
+namespace Main.BL.Converters;
+
+public static class ChatSyncConverter
+{
+    public static ChatSync ToSync(this Chat chat, List<User> users, List<Message> messages)
+    {
+        return new ChatSync
+        {
+            ChatMeta = new ChatMeta
+            {
+                Id = chat.Id,
+                Name = chat.Name,
+                Type = chat.Type,
+                OwnerUserId = chat.OwnerUserId,
+                CreatedAt = chat.CreatedAt,
+                Version = chat.Version,
+                LastMessageNum = chat.LastMessageNum
+            },
+            Participants = chat.Participants
+                .Select(p =>
+                {
+                    var user = users.First(u => u.Id == p.UserId);
+                    return new ChatParticipantInfo
+                    {
+                        UserId = user.Id,
+                        UniqueName = user.UniqueName,
+                        DisplayedName = user.DisplayedName,
+                        LastMessageRead = p.LastMessageRead
+                    };
+                }).ToList(),
+            Messages = messages
+        };
+    }
+}
---
+++ src/backend/Main.BL/InPorts/ISyncService.cs
@@ -0,0 +1,8 @@
+﻿using Main.BL.Models;
+namespace Main.BL.InPorts;
+
+public interface ISyncService
+{
+    Task<ChatSync> SyncChatAsync(Guid ChatId, ulong clientVersion, Guid currentUserId);
+    Task<List<ChatSync>> SyncChatsAsync(List<(Guid, ulong)> Chats, Guid currentUserId);
+}
