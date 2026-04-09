d863db6cbf94b7fcd3a633799cb06c0c2df06a4d
Ktot17 <chikinpavel4@gmail.com>
2026-04-09 18:47:23 +0300

    #25 Реализовал бизнес логику

--- src/backend/Realtime.BL/InputPorts/IGroupManager.cs
+++ src/backend/Realtime.BL/InputPorts/IGroupManager.cs
@@ -2,7 +2,7 @@
 
 public interface IGroupManager
 {
-    Task SendToGroupAsync(string groupName, string senderId, string eventType, string messageJson);
+    Task SendToGroupAsync(string groupName, string eventType, string messageJson);
     Task SendToAllUserGroupsAsync(string userId,  string eventType, string messageJson);
     Task AddToGroupAsync(string connectionId, string groupName);
     Task RemoveFromGroupAsync(string connectionId, string groupName);
---
+++ src/backend/Realtime.BL/MessageHandler.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Realtime.BL.InputPorts;
+using Realtime.BL.OutputPorts;
+using Shared.Main.Realtime;
+using Shared.Main.Realtime.Models;
+
+namespace Realtime.BL;
+
+public class MessageHandler(IConnectionsRepository repository, IGroupManager groupManager) : IMessageHandler
+{
+    public async Task OnMessageReceivedFromBrokerAsync(EventType eventType, string dataJson)
+    {
+        switch (eventType)
+        {
+            case EventType.MessageReceived or EventType.MessageUpdated:
+                var message = JsonConvert.DeserializeObject<MessageDto>(dataJson);
+                
+                if (message is null)
+                    throw new NullReferenceException();
+                
+                await groupManager.SendToGroupAsync(message.ChatId.ToString(), eventType.ToString(), dataJson);
+                break;
+            case EventType.UserChanged:
+                var user = JsonConvert.DeserializeObject<UserDto>(dataJson);
+                
+                if (user is null)
+                    throw new NullReferenceException();
+                
+                await groupManager.SendToAllUserGroupsAsync(user.Id.ToString(), eventType.ToString(), dataJson);
+                break;
+            case EventType.MessageRead or EventType.ChatUserJoined or EventType.ChatUserLeft:
+                var chatUser = JsonConvert.DeserializeObject<ChatUserDto>(dataJson);
+                
+                if (chatUser is null)
+                    throw new NullReferenceException();
+                
+                var connections = repository.GetConnectionIdsByUserId(chatUser.User.Id.ToString());
+                foreach (var connectionId in connections)
+                    if (eventType is EventType.ChatUserJoined)
+                        await groupManager.AddToGroupAsync(connectionId, chatUser.ChatId.ToString());
+                    else
+                        await groupManager.RemoveFromGroupAsync(connectionId, chatUser.ChatId.ToString());
+                
+                await groupManager.SendToGroupAsync(chatUser.ChatId.ToString(), eventType.ToString(), dataJson);
+                break;
+            case EventType.ChatUpdated:
+                var chat = JsonConvert.DeserializeObject<ChatDto>(dataJson);
+                
+                if (chat is null)
+                    throw new NullReferenceException();
+                
+                await groupManager.SendToGroupAsync(chat.Id.ToString(), eventType.ToString(), dataJson);
+                break;
+            case EventType.ChatCreated:
+                var newChat = JsonConvert.DeserializeObject<NewChatDto>(dataJson);
+                
+                if (newChat is null)
+                    throw new NullReferenceException();
+
+                foreach (var connectionId in newChat.Participants.Select(participant => 
+                             repository.GetConnectionIdsByUserId(participant.User.Id.ToString()))
+                             .SelectMany(x => x))
+                    await groupManager.AddToGroupAsync(connectionId, newChat.Chat.Id.ToString());
+                
+                await groupManager.SendToGroupAsync(newChat.Chat.Id.ToString(), eventType.ToString(), dataJson);
+                break;
+        }
+    }
+}
