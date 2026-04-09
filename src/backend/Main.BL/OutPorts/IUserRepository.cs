53c7459dafe9d539f67108bc851e5eeb8d8e7866
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-09 11:55:09 +0300

    #21 репозиторий для user

---
+++ src/backend/Main.BL/OutPorts/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Main.BL.Models;
+
+namespace Main.BL.OutPorts;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByUniqueNameAsync(string uniqueName);
+    Task<bool> ExistsAsync(Guid id);
+    Task<bool> ExistsByUniqueNameAsync(string uniqueName);
+    Task<bool> CreateAsync(User user);
+    Task<bool> UpdateDisplayedNameAsync(Guid id, string newDisplayedName);
+    Task<bool> DeleteAsync(Guid id);
+}
