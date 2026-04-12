68fbcd897d3384e9b21e69c3fd1e2bb78e464f02
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-12 16:10:53 +0300

    #21 сервис пользователей

--- src/backend/Main.BL/InPorts/IUserService.cs
+++ src/backend/Main.BL/InPorts/IUserService.cs
@@ -5,5 +5,5 @@ public interface IUserService
 {
     Task<User> GetUserByIdAsync(Guid userId, Guid currentUserId);
     Task<User> GetMyProfileAsync(Guid currentUserId);
-    Task<IEnumerable<User>> SearchUsersAsync(string? substr, int maxUsers, Guid currentUserId);
+    Task<IEnumerable<User>> SearchUsersAsync(string substr, int maxUsers, Guid currentUserId);
 }
--- src/backend/Main.BL/OutPorts/IUserRepository.cs
+++ src/backend/Main.BL/OutPorts/IUserRepository.cs
@@ -6,6 +6,7 @@ public interface IUserRepository
 {
     Task<User?> GetByIdAsync(Guid id);
     Task<User?> GetByUniqueNameAsync(string uniqueName);
+    Task<IEnumerable<User>> SearchAsync(string substr, int maxUsers);
     Task<bool> ExistsAsync(Guid id);
     Task<bool> ExistsByUniqueNameAsync(string uniqueName);
     Task<bool> CreateAsync(User user);
