4da9f82f7989500a1d884f1181278a4233da9f5a
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-15 18:09:05 +0300

    #21 добавил dto для сообщения и пользователя в application слое

---
+++ src/backend/Main.Application/Dtos/MessageDto.cs
@@ -0,0 +1,18 @@
+﻿using Main.Application.Enums;
+namespace Main.Application.Dtos;
+
+public class MessageDto
+{
+    public ulong MessageNumber { get; init; }
+    public Guid ChatId { get; init; }
+    public Guid? SenderUserId { get; init; }
+    public string Text { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime? EditedAt { get; init; }
+    public bool Deleted { get; init; }
+    public ulong Version { get; init; }
+    public MessageTypeApp Type { get; init; }
+    public ulong? ReplyToMessageNumber { get; init; }
+    public Guid? ForwardedFromUserId { get; init; }
+}
+
