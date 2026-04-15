62e94243e55fe32aa60fb4248fb49656db9c4d88
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-15 16:07:18 +0300

    #21 добавил в сервис пользователей метод создания пользователя

--- src/backend/Main.Application/InPorts/IUserService.cs
+++ src/backend/Main.Application/InPorts/IUserService.cs
@@ -4,5 +4,6 @@ public interface IUserService
 {
     Task<User> GetUserByIdAsync(Guid userId, Guid currentUserId);
     Task<User> GetMyProfileAsync(Guid currentUserId);
+    Task<User> CreateUserAsync(string uniqueName, string displayedName);
     Task<IEnumerable<User>> SearchUsersAsync(string substr, int maxUsers, Guid currentUserId);
 }
