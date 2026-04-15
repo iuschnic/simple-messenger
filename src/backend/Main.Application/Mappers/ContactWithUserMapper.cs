cb005ace508590008a898e0f4bafb98fe9b9ce56
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-15 18:47:51 +0300

    #21 добавил мапперы из бизнес логики в Dto

---
+++ src/backend/Main.Application/Mappers/ContactWithUserMapper.cs
@@ -0,0 +1,19 @@
+﻿using Main.Application.Dtos;
+using Main.BL.Models;
+namespace Main.Application.Mappers;
+
+public static class ContactWithUserMapper
+{
+    public static ContactWithUserDto ToDto(this Contact domain, User user)
+    {
+        if (domain.ContactUserId != user.Id)
+            throw new ArgumentException(
+                $"User Id mismatch: Contact.ContactUserId ({domain.ContactUserId}) " +
+                $"does not match User.Id ({user.Id})");
+        return new ContactWithUserDto
+        {
+            ContactUser = user,
+            ContactName = domain.ContactName
+        };
+    }
+}
---
+++ src/backend/Main.Application/Mappers/MessageMapper.cs
@@ -0,0 +1,26 @@
+﻿using Main.Application.Enums;
+using Main.BL.Models;
+using Main.Application.Dtos;
+
+namespace Main.Application.Mappers;
+
+public static class MessageMapper
+{
+    public static MessageDto ToDto(this Message domain)
+    {
+        return new MessageDto
+        {
+            MessageNumber = domain.MessageNumber,
+            ChatId = domain.ChatId,
+            SenderUserId = domain.SenderUserId,
+            Text = domain.Text,
+            CreatedAt = domain.CreatedAt,
+            EditedAt = domain.EditedAt,
+            Deleted = domain.Deleted,
+            Version = domain.Version,
+            Type = (MessageTypeApp) domain.Type,
+            ReplyToMessageNumber = domain.ReplyToMessageNumber,
+            ForwardedFromUserId = domain.ForwardedFromUserId
+        };
+    }
+}
