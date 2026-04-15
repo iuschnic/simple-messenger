37fdc6128bb08a4a39e750b62147ce7f8933b11d
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-15 15:45:22 +0300

    #21 доделал сервис чатов

--- src/backend/Main.Application/InPorts/IChatService.cs
+++ src/backend/Main.Application/InPorts/IChatService.cs
@@ -1,11 +1,11 @@
-﻿using Main.BL.Models;
+﻿using Main.Application.Dtos;
 namespace Main.Application.InPorts;
 public interface IChatService
 {
-    Task<IEnumerable<Chat>> GetChatsAsync(Guid currentUserId);
-    Task<Chat> GetChatByIdAsync(Guid chatId, Guid currentUserId);
-    Task<Chat> CreatePrivateChatAsync(Guid otherUserId, Guid currentUserId);
-    Task<Chat> CreateGroupChatAsync(string name, List<Guid> memberIds, Guid currentUserId);
+    Task<IEnumerable<ChatWithUsers>> GetChatsAsync(Guid currentUserId);
+    Task<ChatWithUsers> GetChatByIdAsync(Guid chatId, Guid currentUserId);
+    Task<Guid> CreatePrivateChatAsync(Guid otherUserId, Guid currentUserId);
+    Task<Guid> CreateGroupChatAsync(string name, List<Guid> memberIds, Guid currentUserId);
     Task UpdateChatNameAsync(Guid chatId, string newName, Guid currentUserId);
     Task AddMemberAsync(Guid chatId, Guid userIdToAdd, Guid currentUserId);
     Task RemoveMemberAsync(Guid chatId, Guid userIdToRemove, Guid currentUserId);
