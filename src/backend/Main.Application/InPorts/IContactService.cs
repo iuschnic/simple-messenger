1314017d1ed17cb1822f3f44a3833587b3f52be8
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-15 16:01:56 +0300

    #21 в сервисе контактов теперь возвращается dto при изменении/создании контакта

--- src/backend/Main.Application/InPorts/IContactService.cs
+++ src/backend/Main.Application/InPorts/IContactService.cs
@@ -3,7 +3,7 @@ namespace Main.Application.InPorts;
 public interface IContactService
 {
     Task<IEnumerable<ContactWithUser>> GetMyContactsAsync(Guid userId);
-    Task AddContactAsync(Guid ownerUserId, Guid contactUserId, string contactName);
-    Task ChangeContactNameAsync(Guid ownerUserId, Guid contactUserId, string newContactName);
+    Task<ContactWithUser> AddContactAsync(Guid ownerUserId, Guid contactUserId, string contactName);
+    Task<ContactWithUser> ChangeContactNameAsync(Guid ownerUserId, Guid contactUserId, string newContactName);
     Task RemoveContactAsync(Guid ownerUserId, Guid contactUserId);
 }
