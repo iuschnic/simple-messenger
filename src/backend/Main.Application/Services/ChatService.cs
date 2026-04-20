7ac2b578eb674d3bbed63859691f2981da7400d7
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-04-20 12:30:22 +0300

    #21 добавил генерацию системных сообщений в ChatService, добавил валидацию группового чата на операции связанные только с ним

--- src/backend/Main.Application/Services/ChatService.cs
+++ src/backend/Main.Application/Services/ChatService.cs
@@ -3,6 +3,7 @@ using Main.Application.InPorts;
 using Main.Application.OutPorts;
 using Main.BL.Exceptions;
 using Main.BL.Models;
+using Main.BL.Enums;
 
 namespace Main.Application.Services;
 
@@ -42,7 +43,7 @@ public class ChatService: BaseService, IChatService
     public async Task<ChatWithUsersDto> GetChatByIdAsync(Guid chatId, Guid currentUserId)
     {
         await EnsureUserExists(currentUserId);
-        var chat = await _chatRepo.GetByIdAsync(chatId);
+        var chat = await GetChatOrThrow(chatId);
         var userIds = chat.Participants.Select(p => p.UserId).ToList();
         var users = await _userRepo.GetByIdsAsync(userIds);
         var userMap = users.ToDictionary(u => u.Id);
@@ -75,71 +76,129 @@ public class ChatService: BaseService, IChatService
         await _chatRepo.CreateAsync(chat);
         return chat.Id;
     }
+
     public async Task<Guid> CreateGroupChatAsync(string name, List<Guid> memberIds, Guid currentUserId)
     {
-        var allUserIds = memberIds.Append(currentUserId).Distinct();
-        foreach (var userId in allUserIds)
-            await EnsureUserExists(userId);
+        var allUserIds = memberIds
+            .Append(currentUserId)
+            .Distinct()
+            .ToList();
+        var members = await _userRepo.GetByIdsAsync(allUserIds);
+        var foundIds = members.Select(x => x.Id).ToHashSet();
+        var notFound = allUserIds.Where(id => !foundIds.Contains(id)).ToList();
+
+        if (notFound.Count != 0)
+            throw new NotFoundException(nameof(User), notFound[0]);
 
         if (string.IsNullOrWhiteSpace(name))
             throw new RuleViolationException("Group chat must have a name");
 
-        var participants = allUserIds.Select(userId => new ChatUser(userId, 0)).ToList();
+        var userMap = members.ToDictionary(m => m.Id);
+        var participants = allUserIds
+            .Select(userId => new ChatUser(userId, 0))
+            .ToList();
         var chat = Chat.CreateGroup(name, currentUserId, participants);
         await _chatRepo.CreateAsync(chat);
+
+        var creatorName = userMap[currentUserId].DisplayedName;
+        await _messageRepo.TryCreateAsync(
+            Message.CreateSystem(
+                chat.Id,
+                $"{creatorName} создал чат"
+            )
+        );
+        foreach (var userId in memberIds.Where(id => id != currentUserId).Distinct())
+        {
+            await _messageRepo.TryCreateAsync(
+                Message.CreateSystem(
+                    chat.Id,
+                    $"{userMap[userId].DisplayedName} вступил в чат"
+                )
+            );
+        }
         return chat.Id;
     }
     public async Task UpdateChatNameAsync(Guid chatId, string newName, Guid currentUserId)
     {
-        await EnsureUserExists(currentUserId);
-
+        var user = await GetUserOrThrow(currentUserId);
         var chat = await GetChatOrThrow(chatId);
+        EnsureGroupChat(chat);
         EnsureOwner(chat, currentUserId);
-
         if (!await _chatRepo.TryUpdateNameAsync(chatId, newName))
             throw new TechnicalException("Failed to update chat name");
+        await _messageRepo.TryCreateAsync(
+                Message.CreateSystem(
+                    chat.Id,
+                    $"{user.DisplayedName} изменил(а) название группы на \"{newName}\""
+                )
+            );
     }
     public async Task AddMemberAsync(Guid chatId, Guid userIdToAdd, Guid currentUserId)
     {
-        await EnsureUserExists(currentUserId);
-        await EnsureUserExists(userIdToAdd);
-        await EnsureChatExists(chatId);
+        var currentUser = await GetUserOrThrow(currentUserId);
+        var toAddUser = await GetUserOrThrow(userIdToAdd);
+        var chat = await GetChatOrThrow(chatId);
+        EnsureGroupChat(chat);
         await EnsureParticipant(chatId, currentUserId);
         await EnsureNotParticipant(chatId, userIdToAdd);
 
         var participant = new ChatUser(userIdToAdd, 0);
-
         if (!await _chatUserRepo.TryAddAsync(chatId, participant))
             throw new TechnicalException("Failed to add new member");
+
+        await _messageRepo.TryCreateAsync(
+                Message.CreateSystem(
+                    chatId,
+                    $"{currentUser.DisplayedName} добавил(а) {toAddUser.DisplayedName}"
+                )
+            );
     }
     public async Task RemoveMemberAsync(Guid chatId, Guid userIdToRemove, Guid currentUserId)
     {
-        await EnsureUserExists(currentUserId);
-        await EnsureUserExists(userIdToRemove);
+        var currentUser = await GetUserOrThrow(currentUserId);
+        var toRemoveUser = await GetUserOrThrow(userIdToRemove);
         var chat = await GetChatOrThrow(chatId);
+        EnsureGroupChat(chat);
         await EnsureParticipant(chatId, currentUserId);
         await EnsureParticipant(chatId, userIdToRemove);
-
         EnsureOwner(chat, currentUserId);
 
         if (userIdToRemove == chat.OwnerUserId)
             throw new RuleViolationException("Cannot remove chat owner");
-
         if (userIdToRemove == currentUserId)
             throw new RuleViolationException("Use LeaveChat to leave the chat");
 
         if (!await _chatUserRepo.TryRemoveAsync(chatId, userIdToRemove))
             throw new TechnicalException("Failed to remove member");
+
+        await _messageRepo.TryCreateAsync(
+                Message.CreateSystem(
+                    chatId,
+                    $"{currentUser.DisplayedName} удалил(а) {toRemoveUser.DisplayedName}"
+                )
+            );
     }
     public async Task LeaveChatAsync(Guid chatId, Guid currentUserId)
     {
-        await EnsureUserExists(currentUserId);
+        var currentUser = await GetUserOrThrow(currentUserId);
         var chat = await GetChatOrThrow(chatId);
+        EnsureGroupChat(chat);
         await EnsureParticipant(chatId, currentUserId);
-
         EnsureNotOwner(chat, currentUserId);
 
         if (!await _chatUserRepo.TryRemoveAsync(chatId, currentUserId))
             throw new TechnicalException("Failed to remove member");
+
+        await _messageRepo.TryCreateAsync(
+                Message.CreateSystem(
+                    chatId,
+                    $"{currentUser.DisplayedName} покинул(а) группу"
+                )
+            );
+    }
+    private void EnsureGroupChat(Chat chat)
+    {
+        if (chat.Type != ChatType.Group)
+            throw new RuleViolationException("Chat is not private");
     }
 }
