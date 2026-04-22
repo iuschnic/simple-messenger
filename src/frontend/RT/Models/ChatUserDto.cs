88002c65e362af3daab9c1b97fdea6622cc52506
Ktot17 <chikinpavel4@gmail.com>
2026-04-22 19:55:53 +0300

    #37 Добавил модуль обработки событий

---
+++ src/frontend/RT/Models/ChatUserDto.cs
@@ -0,0 +1,8 @@
+﻿using Newtonsoft.Json;
+
+namespace RT.Models;
+
+public record ChatUserDto(
+    [JsonProperty] Guid ChatId,
+    [JsonProperty] UserDto User, 
+    [JsonProperty] ulong LastMessageRead);
